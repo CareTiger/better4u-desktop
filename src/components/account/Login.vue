6a0fe771da058a785de2996cbf6431318bbfce9a
venkatesh <currency@gmail.com>
2020-01-04 07:12:07 -0600

    updates

--- src/components/account/Login.vue
+++ src/components/account/Login.vue
@@ -1,19 +1,27 @@
 <template>
     <div class="ma4">
 
-        <h3>Login</h3>
+        <h1 class="f3">Login</h1>
 
-        <form action="">
-            <div class="measure">
-                <label for="email" class="f6 b db mb2">Email</label>
-                <input id="email" class="" type="email" placeholder="Email" v-model="email">
-            </div>
-            <div class="pa1 mt2">
-                <label class="dib">Password</label>
-                <input type="password" placeholder="Password" v-model="password">
-            </div>            
-            <a class="f6 dark-green no-underline ba grow pv2 ph4 dib" @click.prevent="Login">Login</a>
-        </form>        
+        <div v-if="!success">
+            <form action="">
+                <div class="measure mb3">
+                    <label for="email" class="f6 b db mb2">Email <span class="normal black-60">(required)</span></label>
+                    <input id="email" class="input-reset ba b--black-20 pa2 mb1 db w-100" type="email" placeholder="Email" v-model="email">
+                </div>
+                <div class="measure mb3">
+                    <label for="password" class="f6 b db mb2">Password <span class="normal black-60">(required)</span></label>
+                    <input id="password" class="input-reset ba b--black-20 pa2 mb1 db w-100" type="password" placeholder="Password" v-model="password">
+                </div>
+                <div class="measure mb2">
+
+                </div>
+                <a class="f6 dark-green no-underline ba grow pv2 ph4 dib" @click.prevent="Login">Login</a>
+            </form>        
+        </div>
+        <div v-else>
+            <h5 class="f4 green">Yaay! You logged in successfully.</h5>
+        </div>
 
     </div>
 </template>
@@ -27,13 +35,14 @@ export default {
     data(){
         return{
             email: '',
-            password: ''
+            password: '',
+            success: false
         }
     },
     methods: {
         async Login(){
-                console.log("register")            
-                await auth.createUserWithEmailAndPassword(this.email, this.password).then((response) => {
+                this.success = true
+                await auth.signInWithEmailAndPassword(this.email, this.password).then((response) => {
             }).catch((err) => {
                 console.log(err);
             })
